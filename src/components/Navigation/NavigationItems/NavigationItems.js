5a9880a8c2733cc1d10dd1a48ebd217aca40e22c
ASLI\aslig <asligunay@me.com>
2021-01-27 23:57:35 +0000

    fix

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,18 +3,14 @@ import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const NavigationItems = (props) => (
+const navigationItems = ( props ) => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/">Burger Builder</NavigationItem>
-        { props.isAuthenticated 
-            ? <NavigationItem link="/orders">Orders</NavigationItem>
-            : null
-        }
-        { props.isAuthenticated 
-            ? <NavigationItem link="/logout">Logout</NavigationItem>
-            : <NavigationItem link="/auth">Authenticate</NavigationItem>
-        }
+        <NavigationItem link="/" exact>Burger Builder</NavigationItem>
+        {props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
+        {!props.isAuthenticated
+            ? <NavigationItem link="/auth">Authenticate</NavigationItem>
+            : <NavigationItem link="/logout">Logout</NavigationItem>}
     </ul>
 );
 
-export default NavigationItems;
+export default navigationItems;
