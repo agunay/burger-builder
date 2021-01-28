2e84b1893abbba1b0229ea71f8f4625c7111f2cd
ASLI\aslig <asligunay@me.com>
2021-01-28 00:15:39 +0000

    firebase hosting

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -7,7 +7,7 @@ import Button from '../../UI/Button/Button';
 const CheckoutSummary = (props) => {
     return (
         <div className={classes.CheckoutSummary}>
-            <h1>We hope it tastes well!</h1>
+            <h1>Hope it tastes well!</h1>
             <div style={{width: '100%', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients} />
             </div>
